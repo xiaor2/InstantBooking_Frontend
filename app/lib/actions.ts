ce38a4192d4ab5a1b62b76de4a52a0bdd656783b
xiaor2 <xiaor2@illinois.edu>
2024-04-30 23:32:50 -0500

    Temporary fixed the security issue.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -46,21 +46,24 @@ export async function handleRefresh() {
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
     cookies().set('session_userid', userId, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 60 * 60 * 24 * 7,
         path: '/'
     });
 
     cookies().set('session_access_token', accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 60 * 60,
         path: '/'
     });
 
     cookies().set('session_refresh_token', refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 60 * 60 * 24 * 7,
         path: '/'
     })   
